1230d06397e873409b9f14ab837f669673fdcf35
Rohith K V <rohith.kv@planview.com>
2024-08-28 12:13:08 +0530

    additionapp

---
+++ SampleSolution/MyApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyApp1;
+
+Console.WriteLine("Hello, World!");
+
+
+Calculator calculator = new Calculator();
+
+int res=calculator.add(4, 5);
+Console.WriteLine(res);
+Console.ReadLine();
