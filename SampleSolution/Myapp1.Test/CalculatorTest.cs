02da5b18c387b9e50f57bfbe33f7d78898cbb6e1
Rohith K V <rohith.kv@planview.com>
2024-09-02 10:05:35 +0530

    adding new solution

---
+++ SampleSolution/Myapp1.Test/CalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyApp1;
+
+namespace Myapp1.Test
+{
+    public class CalculatorTest
+    {
+        [Test]
+        public void add_Test()
+        {
+            //Arrange
+            Calculator calculator = new Calculator();
+
+            //Act
+            int result = calculator.add(2, 3);
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
+
+    }
+}
---
+++ SampleSolution/Myapp1.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Myapp1.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
